e41010cd34ce64209939f6c21de9375e5a8a2ed1
Aziz-Matkawala <66515676+Aziz-Matkawala@users.noreply.github.com>
2020-06-22 11:42:41 +0530

    uploaded the whole project

---
+++ ball.js
@@ -0,0 +1,26 @@
+class Ball{
+    constructor(x, y){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body = Bodies.circle(x, y, 10, options);
+        this.radius = 10;
+        
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle)
+        ellipseMode(RADIUS);
+        fill("purple");
+        ellipse(0, 0, this.radius);
+        pop();
+      }
+}
